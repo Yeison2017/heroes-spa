1caa65d64538cde7bfb9642ca1d22e84a72028c9
Yeison <yha.big@gmail.com>
2022-12-15 23:08:17 -0500

    Estilo del componente HeroScreen

--- src/heroes/helpers/getHeroById.ts
+++ src/heroes/helpers/getHeroById.ts
@@ -1,5 +1,6 @@
 import { heroes } from "../data/heroes";
+import { IHero } from "../data/heroes";
 
-export const getHeroById = (id: string) => {
+export const getHeroById = (id: string): IHero | undefined => {
     return heroes.find((hero) => hero.id === id);
 };
