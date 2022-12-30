9860019f29ac784bbd08ce0d3739a6a64829cdfd
yeison2017 <yha.big@gmail.com>
2022-12-30 09:04:19 -0500

    Muestra sugerencias al escribir

--- src/heroes/components/HeroList.tsx
+++ src/heroes/components/HeroList.tsx
@@ -1,26 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getHeroesByPublisher } from "../helpers/getHeroesByPublisher";
 import { IHero } from "../data/heroes";
 import HeroCard from "./HeroCard";
 
 interface IHeroListProps {
-    publisher: string;
+  publisher: string;
 }
 
 const HeroList = ({ publisher }: IHeroListProps) => {
-    const [heroes, setHeroes] = useState<IHero[]>([]);
+  // const [heroes, setHeroes] = useState<IHero[]>([]);
 
-    useEffect(() => {
-        setHeroes(getHeroesByPublisher(publisher));
-    }, []);
+  // useEffect(() => {
+  //     setHeroes(getHeroesByPublisher(publisher));
+  // }, []);
 
-    return (
-        <div className="row rows-cols-1 row-cols-md-3 g-3">
-            {heroes.map((hero: IHero) => (
-                <HeroCard key={hero.id} {...hero} />
-            ))}
-        </div>
-    );
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), []);
+
+  return (
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
+      {heroes.map((hero: IHero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
+    </div>
+  );
 };
 
 export default HeroList;
