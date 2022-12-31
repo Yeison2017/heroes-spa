ae6c5adf80f15e712f4e56b0d1a78b464ce53766
yeison2017 <yha.big@gmail.com>
2022-12-31 18:32:15 -0500

    Mostrar correctamente con valores por defecto en SearchPage

---
+++ test/heroes/pages/SearchPage.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import SearchPage from "../../../src/heroes/pages/SearchPage";
+
+describe("Pruebas en <SearchPage />", () => {
+  test("debe de mostrarse correctamente con valores por defecto", () => {
+    const { container } = render(
+      <MemoryRouter>
+        <SearchPage />
+      </MemoryRouter>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
